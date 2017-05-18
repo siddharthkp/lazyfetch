4b285de6016172a28d0e67e1e7d97d7327334ee6
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-05-18 13:00:38 +0530

    everything

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # lazyfetch
-Load stuff lazily on the browser
+
+Load stuff lazily on the browser.
+
+&nbsp;
+
+- only 366 bytes
+- ignored on the server (relevant for server side rendering)
+
+&nbsp;
+
+### Usage
+
+`npm install lazyfetch --save`
+
+```
+import lazyfetch from 'lazyfetch'
+
+lazyfetch(
+  /* list of urls to load */
+  [
+    'https://example.com/script.js',
+    'https://example.com/styles.css'
+  ],
+  /* callback to execute after all of the urls have been fetched */
+  () => {
+    // ...
+  }
+)
+```
+
+or load in the browser
+
+```
+<script async src="https://unpkg.com/lazyfetch/lib.js"></script>
+```
+
+&nbsp;
+
+### like it?
+
+:star: this repo
+
+&nbsp;
+
+### license
+
+MIT © [siddharthkp](https://github.com/siddharthkp)
---
+++ index.js
@@ -0,0 +1,48 @@
+const lazyfetch = (urls, callback, timeout) => {
+  /* don't load on the server */
+  if (typeof window == 'undefined') return
+
+  /* default timeout of 3s */
+  if (!timout) timeout = 3000
+
+  /*
+    lazyfetch also supports single urls as well,
+    convert that to an array for consistency
+  */
+  if (typeof urls === 'string') urls = [urls]
+
+  /*
+    we need to wait for all the urls to be loaded
+    before executing the callback
+  */
+  const count = urls.length
+  let loaded = 0
+  const callbackHandler = () => {
+    loaded++
+    if (loaded === count && callback) callback()
+  }
+
+  window.setTimeout(() => {
+    urls.map(url => {
+      const extension = url.split('.').pop()
+
+      let tag
+
+      if (extension === 'js') {
+        tag = document.createElement('script')
+        tag.src = url
+      } else if (extension === 'css') {
+        tag = document.createElement('link')
+        tag.href = url
+        tag.rel = 'stylesheet'
+        tag.type = 'text/css'
+      }
+      tag.onload = callbackHandler
+
+      if (['js', 'css'].includes(extension)) document.head.appendChild(tag)
+      else console.log(`lazy load does not know how to handle ${extension}`)
+    })
+  }, timeout)
+}
+
+module.exports = lazyfetch
