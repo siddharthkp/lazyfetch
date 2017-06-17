08d8662705549e42292511cc79a20045e0ad25a4
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-06-17 15:20:44 +0530

    support multiple instances of lazyfetch

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const lazyfetchUrls = []
+
 const lazyfetch = (urls, callback) => {
   /* don't load on the server */
   if (typeof window == 'undefined') return
@@ -8,18 +10,23 @@ const lazyfetch = (urls, callback) => {
   */
   if (typeof urls === 'string') urls = [urls]
 
-  /*
+  /* put them in the global array */
+  lazyfetchUrls.push({ urls, callback })
+}
+
+const loadUrls = () => {
+  lazyfetchUrls.map(({ urls, callback }) => {
+    /*
     we need to wait for all the urls to be loaded
     before executing the callback
   */
-  const count = urls.length
-  let loaded = 0
-  const callbackHandler = () => {
-    loaded++
-    if (loaded === count && callback) callback()
-  }
+    const count = urls.length
+    let loaded = 0
+    const callbackHandler = () => {
+      loaded++
+      if (loaded === count && callback) callback()
+    }
 
-  const loadUrls = () => {
     urls.map(url => {
       const extension = url.split('.').pop()
 
@@ -39,7 +46,10 @@ const lazyfetch = (urls, callback) => {
       if (['js', 'css'].includes(extension)) document.head.appendChild(tag)
       else console.warn(`lazyfetch does not know how to handle ${extension}`)
     })
-  }
+  })
+}
+
+if (typeof window !== 'undefined') {
   if (document.readyState === 'complete') loadUrls()
   else window.onload = loadUrls
 }
